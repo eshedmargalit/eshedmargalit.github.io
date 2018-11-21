0026892352d8e079bdeb0788d166497276f1a712
Eshed Margalit <eshed.margalit@gmail.com>
2018-11-21 08:41:50 -0800

    trying HashRouter to avoid 404s

---
+++ precache-manifest.8cbd4ec422fa8c1d695eb4a797427b2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d862b98dc00fe7d613ec",
+    "url": "/static/css/main.5fbf2a32.chunk.css"
+  },
+  {
+    "revision": "d862b98dc00fe7d613ec",
+    "url": "/static/js/main.d862b98d.chunk.js"
+  },
+  {
+    "revision": "33eee7673fb7feb2a669",
+    "url": "/static/css/1.d4f26f45.chunk.css"
+  },
+  {
+    "revision": "33eee7673fb7feb2a669",
+    "url": "/static/js/1.33eee767.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "79140d493064222ebe0eb239eca9a18b",
+    "url": "/index.html"
+  }
+];
