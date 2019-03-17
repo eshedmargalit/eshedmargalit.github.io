a6e932d353632a2eff5878961e96a10f89b88b82
Eshed Margalit <eshed.margalit@gmail.com>
2019-03-17 09:04:31 -0700

    Updates

---
+++ precache-manifest.dabbb6d1a83f2f6a8c8549e90356338c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "8779d3d8c490314d714a",
+    "url": "/static/css/main.5c4c2b3a.chunk.css"
+  },
+  {
+    "revision": "8779d3d8c490314d714a",
+    "url": "/static/js/main.8779d3d8.chunk.js"
+  },
+  {
+    "revision": "e9fbbc37b187629f7f24",
+    "url": "/static/css/1.40daf364.chunk.css"
+  },
+  {
+    "revision": "e9fbbc37b187629f7f24",
+    "url": "/static/js/1.e9fbbc37.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3afa244f15cf781087aa3cdf7a55982e",
+    "url": "/static/media/snail-sq.3afa244f.png"
+  },
+  {
+    "revision": "c5822a317c55c1a43dffec0fc9372801",
+    "url": "/static/media/vpnl.c5822a31.png"
+  },
+  {
+    "revision": "837e031fc5f3daa16be886cabc77213a",
+    "url": "/static/media/clouds_rest.837e031f.png"
+  },
+  {
+    "revision": "479e4fef90c98e8b6f4d7f11acf56d98",
+    "url": "/static/media/headshot-sq.479e4fef.png"
+  },
+  {
+    "revision": "99b1e0085479462dc16e0745709e3f5d",
+    "url": "/static/media/vtc_fade.99b1e008.gif"
+  },
+  {
+    "revision": "70ef3ddfc68ecdf2ea5d36cb3de12cda",
+    "url": "/static/media/SU_Seal_Red.70ef3ddf.png"
+  },
+  {
+    "revision": "f3cde9dd5a69027d08994bbccba6209b",
+    "url": "/static/media/USC_Seal.f3cde9dd.png"
+  },
+  {
+    "revision": "421510d613403a511b2fd18aa46557d2",
+    "url": "/static/media/emcog.421510d6.jpg"
+  },
+  {
+    "revision": "028dad471557a805a095fa9caa695a2b",
+    "url": "/static/media/LOC_mricrogl.028dad47.png"
+  },
+  {
+    "revision": "91f0ab2e01a7bf85ccd75b53fa9447ce",
+    "url": "/static/media/Margalit_et_al_2017_Fig3.91f0ab2e.png"
+  },
+  {
+    "revision": "7cec74993bfee3266f55aa39e73a2c9b",
+    "url": "/static/media/Margalit_et_al_VSS_2018.7cec7499.png"
+  },
+  {
+    "revision": "81d96289bf9ef50bf654574a54d2c5d8",
+    "url": "/static/media/dissim_histogram.81d96289.png"
+  },
+  {
+    "revision": "754a886a0b1d264684c968d2b85cd340",
+    "url": "/static/media/dp_gui_screenshot.754a886a.png"
+  },
+  {
+    "revision": "95cec5fea5263000f1b0c7acf7bd7338",
+    "url": "/static/media/gabor_grab.95cec5fe.PNG"
+  },
+  {
+    "revision": "4f23cab6ce9292a693ebb357ac7d613f",
+    "url": "/static/media/spacenet.4f23cab6.png"
+  },
+  {
+    "revision": "eae46f3586c1851babab78dee2eaa287",
+    "url": "/static/media/vtc_faces.eae46f35.png"
+  },
+  {
+    "revision": "b8f4b0baac15dc6ff42a01c5deb682dc",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b169e8008b8d6f77fb2441ab89cbc153.js"
+  "/precache-manifest.dabbb6d1a83f2f6a8c8549e90356338c.js"
 );
 
 workbox.clientsClaim();
