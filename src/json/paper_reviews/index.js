b13c07e670a34c40a4993e3f92e15269be697a46
Eshed Margalit <eshed.margalit@gmail.com>
2019-03-28 16:39:46 -0400

    adds ponce livingstone review

--- src/json/paper_reviews/index.js
+++ src/json/paper_reviews/index.js
@@ -2,7 +2,8 @@ const filenames = [
   "koch_pinwheels.json",
   "koulakov_chklovskii_wiring_cost.json",
   "livingstone_hubel_color.json",
-  "nauhaus_orthogonality.json"
+  "nauhaus_orthogonality.json",
+  "ponce_livingstone_super_stimuli.json"
 ];
 
 export default filenames;
