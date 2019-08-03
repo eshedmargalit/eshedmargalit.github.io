f066146fdb8a368a171c4a1e3524c17301d513f1
Eshed Margalit <eshed.margalit@gmail.com>
2019-08-02 23:33:14 -0700

    adds noise distributions and std deviation locking

--- src/components/DPCalc/Calc.js
+++ src/components/DPCalc/Calc.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from "reactstrap";
+import { Form, FormGroup, Label, Input, Container, Row, Col } from "reactstrap";
 import Slider from "react-rangeslider";
-import { TwitterPicker } from "react-color";
 
 import "react-rangeslider/lib/index.css";
 import Chart from "./Chart.js";
@@ -10,12 +9,13 @@ class Calc extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      signal_mean: 5,
-      signal_sigma: 2,
+      signal_mean: 15,
+      signal_sigma: 4,
       signal_color: "#0d5e08",
-      noise_mean: 2,
-      noise_sigma: 3,
-      noise_color: "#960f0f"
+      noise_mean: 10,
+      noise_sigma: 4,
+      noise_color: "#960f0f",
+      sigma_lock: true
     };
   }
 
@@ -26,15 +26,16 @@ class Calc extends Component {
   };
 
   handleOnChangeSignalSigma = value => {
-    this.setState({
-      signal_sigma: value
-    });
-  };
-
-  handleColorChangeSignal = (color, event) => {
-    this.setState({
-      signal_color: color.hex
-    });
+    if (this.state.sigma_lock) {
+      this.setState({
+        signal_sigma: value,
+        noise_sigma: value
+      });
+    } else {
+      this.setState({
+        signal_sigma: value
+      });
+    }
   };
 
   handleOnChangeNoiseMu = value => {
@@ -44,71 +45,108 @@ class Calc extends Component {
   };
 
   handleOnChangeNoiseSigma = value => {
-    this.setState({
-      noise_sigma: value
-    });
+    if (this.state.sigma_lock) {
+      this.setState({
+        noise_sigma: value,
+        signal_sigma: value
+      });
+    } else {
+      this.setState({
+        noise_sigma: value
+      });
+    }
   };
 
-  handleColorChangeNoise = (color, event) => {
+  toggleCheckbox = e => {
     this.setState({
-      noise_color: color.hex
+      sigma_lock: e.target.checked
     });
+    if (e.target.checked) {
+      this.setState({
+        signal_sigma: this.state.noise_sigma
+      });
+    }
   };
 
   render() {
     return (
       <Container>
-        <Chart
-          signal_mean={this.state.signal_mean}
-          signal_sigma={this.state.signal_sigma}
-          signal_color={this.state.signal_color}
-          noise_mean={this.state.noise_mean}
-          noise_sigma={this.state.noise_sigma}
-          noise_color={this.state.noise_color}
-        />
         <Row>
-          <Col lg="4" xs="4">
+          <Col lg="8" xs="8">
+            <Chart
+              height={300}
+              signal_mean={this.state.signal_mean}
+              signal_sigma={this.state.signal_sigma}
+              signal_color={this.state.signal_color}
+              noise_mean={this.state.noise_mean}
+              noise_sigma={this.state.noise_sigma}
+              noise_color={this.state.noise_color}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col lg="5" xs="5">
+            <div>
+              Signal Mean: <strong>{this.state.signal_mean}</strong>
+            </div>
             <Slider
               min={0}
-              max={100}
+              max={25}
               value={this.state.signal_mean}
               orientation="horizontal"
               onChange={this.handleOnChangeSignalMu}
             />
           </Col>
-          <Col lg="4" xs="4">
+          <Col lg="5" xs="5">
+            <div>
+              Signal Std: <strong>{this.state.signal_sigma}</strong>
+            </div>
             <Slider
+              min={0}
+              max={25}
               value={this.state.signal_sigma}
               orientation="horizontal"
               onChange={this.handleOnChangeSignalSigma}
             />
           </Col>
-          <Col lg="4" xs="4">
-            <TwitterPicker
-              color={this.state.signal_color}
-              onChangeComplete={this.handleColorChangeSignal}
-            />
+          <Col lg="2" xs="2">
+            <Form>
+              <FormGroup check>
+                <Label check>
+                  <Input
+                    defaultChecked={this.state.sigma_lock}
+                    onClick={this.toggleCheckbox}
+                    type="checkbox"
+                  />{" "}
+                  Assume equal sigmas?
+                </Label>
+              </FormGroup>
+            </Form>
           </Col>
         </Row>
         <Row>
-          <Col lg="4" xs="4">
+          <Col lg="5" xs="5">
+            <div>
+              Noise Mean: <strong>{this.state.noise_mean}</strong>
+            </div>
             <Slider
+              min={0}
+              max={25}
               value={this.state.noise_mean}
               orientation="horizontal"
               onChange={this.handleOnChangeNoiseMu}
             />
           </Col>
-          <Col lg="4" xs="4">
+          <Col lg="5" xs="5">
+            <div>
+              Noise Std: <strong>{this.state.noise_sigma}</strong>
+            </div>
             <Slider
+              min={0}
+              max={25}
               value={this.state.noise_sigma}
               orientation="horizontal"
               onChange={this.handleOnChangeNoiseSigma}
-            />
-          </Col>
-          <Col lg="4" xs="4">
-            <TwitterPicker
-              color={this.state.noise_color}
-              onChangeComplete={this.handleColorChangeNoise}
             />
           </Col>
         </Row>
