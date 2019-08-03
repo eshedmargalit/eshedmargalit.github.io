38c6039bbafeaa4f8df5a1d78b3736aa3f081a4d
Eshed Margalit <eshed.margalit@gmail.com>
2019-08-02 23:33:14 -0700

    adds noise distributions and std deviation locking

--- src/components/DPCalc/Chart.js
+++ src/components/DPCalc/Chart.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
-import { Container } from "reactstrap";
-import { AreaChart, Area, Legend, Tooltip, XAxis, YAxis } from "recharts";
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  ReferenceLine,
+  Legend,
+  XAxis,
+  YAxis
+} from "recharts";
 
 class Chart extends Component {
   getData() {
     var data = [];
-    for (var i = 0; i < 100; i++) {
+    for (var i = 0; i < 30; i++) {
       var signal = this.signal_gaussian(i);
       var noise = this.noise_gaussian(i);
       var el = {
@@ -39,35 +46,40 @@ class Chart extends Component {
 
   render() {
     return (
-      <Container>
-        <div>
-          <AreaChart
-            width={600}
-            height={300}
-            data={this.getData()}
-            margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-          >
-            <XAxis dataKey="x" />
-            <YAxis />
-            <Area
-              name="signal"
-              type="monotone"
-              dataKey="signal"
-              stroke={this.props.signal_color}
-              fill={this.props.signal_color}
-            />
-            <Area
-              name="noise"
-              type="monotone"
-              dataKey="noise"
-              stroke={this.props.noise_color}
-              fill={this.props.noise_color}
-            />
-            <Tooltip />
-            <Legend verticalAlign="top" height={36} />
-          </AreaChart>
-        </div>
-      </Container>
+      <ResponsiveContainer width="100%" height={this.props.height}>
+        <AreaChart
+          data={this.getData()}
+          margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+        >
+          <XAxis dataKey="x" />
+          <YAxis />
+          <Area
+            name="Signal Distribution"
+            type="monotone"
+            dataKey="signal"
+            stroke={this.props.signal_color}
+            fill={this.props.signal_color}
+          />
+          <ReferenceLine
+            x={this.props.signal_mean}
+            stroke={this.props.signal_color}
+            strokeDasharray="3 3"
+          />
+          <Area
+            name="Noise Distribution"
+            type="monotone"
+            dataKey="noise"
+            stroke={this.props.noise_color}
+            fill={this.props.noise_color}
+          />
+          <ReferenceLine
+            x={this.props.noise_mean}
+            stroke={this.props.noise_color}
+            strokeDasharray="3 3"
+          />
+          <Legend verticalAlign="top" height={36} />
+        </AreaChart>
+      </ResponsiveContainer>
     );
   }
 }
